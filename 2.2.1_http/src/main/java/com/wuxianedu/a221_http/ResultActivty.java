0755d82000a1bf67c9fc3ec87907afb09ff20a3b
zhangkai281 <zhangkai281@sina.com>
2016-09-29 14:59:41 +0800

    增加了ResultActivity

---
+++ 2.2.1_http/src/main/java/com/wuxianedu/a221_http/ResultActivty.java
@@ -0,0 +1,19 @@
+package com.wuxianedu.a221_http;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by caibing.zhang on 2016/9/29.
+ */
+
+public class ResultActivty extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_result);
+    }
+}
